02c8c2a2f5b681cef474dbea19d05dd0ba88af6a
Evangeline Drink <evangelinedrink@gmail.com>
2021-07-29 23:51:02 -0700

    Installed React Router and created Home Component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 //Presentation Content (the visual content) is inside of the App.js file
 import React, {Component} from "react";
 import Main from "./components/MainComponent"; //importing the Main Component
+import {BrowserRouter} from "react-router-dom"; //Importing the BrowserRouter. BrowserRouter is the top-level parent component that wraps around all other React-Router components. React Router is needed to build a Single Page Application (SPA).
 import './App.css';
 
 
 class App extends Component {
   render() { //When we are using (rendering) React components, the syntax will look like HTML or JSX tags with the angle brackets, <>. React components are capitalized, so Navbar and NavbarBarnd are React components.
     return (
-      <div className= "App">
-        <Main /> {/*Rendering (displaying) the Main Component on the web page. */}
-      </div>
+      <BrowserRouter> {/*BrowserRouter is the top-level parent component that wraps around all other React-Router components. React Router is needed to build a Single Page Application (SPA). */}
+        <div className= "App">
+          <Main /> {/*Rendering (displaying) the Main Component on the web page. */}
+        </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/components/HomeComponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+//Home Functional Component that will return a <div> element with the Bootstrap calls of Container
+function Home(props) {
+    return (
+        <div className="container">
+            <h4>Home</h4>
+        </div>
+    );
+} 
+
+export default Home; 
